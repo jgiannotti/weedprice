c169c1710ef9fa496229cf4bd33be07c66792cd9
jgiannotti <joe@floridasoundman.com>
2026-02-10 14:58:46 -0800

    Create LocationBanner.tsx

---
+++ site/src/components/LocationBanner.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const LocationBanner: React.FC = () => {
+  return (
+    <div>
+      <h2>Location Banner</h2>
+    </div>
+  );
+};
+
+export default LocationBanner;
