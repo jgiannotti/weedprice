b483b8ef97e6b2fa80b6d389252682ee59a606c5
jgiannotti <joe@floridasoundman.com>
2026-02-10 16:57:55 -0800

    Create MapPanel.tsx

---
+++ site/src/components/MapPanel.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const MapPanel: React.FC = () => {
+  return (
+    <div className="p-4 border rounded-lg shadow">
+      <h2 className="text-lg font-bold mb-2">Map Panel</h2>
+      <p>Map functionality coming soon. We’ll show dispensaries on a map here.</p>
+    </div>
+  );
+};
+
+export default MapPanel;
